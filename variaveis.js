aa9dce1389638a159e15eb0b4db18363877da488
rogerio123384 <rogerio.carvalho.santos@escola.pr.gov.br>
2026-02-26 12:08:57 -0300

    Add files via upload

---
+++ variaveis.js
@@ -0,0 +1,42 @@
+let a = 40;
+let b = 10;
+let c = 20;
+let total = 0;
+let totald = 0;
+let x = 5;
+total = (a + b + c);
+totald = ( a + b + c ) / x;
+console.log("o total da soma é:", total);
+console.log("o total da soma é :", totald);
+console.log(outracont());
+console.log(media())
+
+//inicio da function
+function outracont() {
+    var d = 10;
+    var e = 20;
+    var f = 30;
+    var tot2 =0;
+    var xx = 100;
+    tot=( d + e + f)
+    tot2 = (d + e + f) / xx;
+   // console.log( "o valor de tot é", tot);
+    //console.log("o valor de tot é", tot2);
+    return tot;
+    return tot2;
+}
+
+
+
+
+
+function media(){
+    let nfinal = 0;
+    n1=30;
+    n2=90;
+    n3=100;
+    nfinal=(n1+ n2 + n3);
+    nfinal=(n1 + n2 + n3) / 3;
+    console.log("A media final de aluno é", nfinal.toFixed(1));
+    return nfinal.toFixed;
+}
