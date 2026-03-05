2f70c0b7b6efc42d3789c8cfb67e3298d487cd7d
rogerio123384 <rogerio.carvalho.santos@escola.pr.gov.br>
2026-03-05 08:01:17 -0300

    Add files via upload

---
+++ carro1.js
@@ -0,0 +1,20 @@
+carro1 = "Fusca";
+km = 100;
+p = 12,70;
+carro2 = "Celta"
+km2 = 360;
+p2 = 45,90;
+console.log("O total do Fusca é:", km * p);
+console.log("O total do Celta é:", km2 * p2);
+console.log(carro(km, km2))
+
+function carro(a, b){
+    carro1 = 'Gol bolinha'
+    carro2 = 'Corsa'
+    p1 = 7,50
+    p2 = 8
+    console.log(carro1)
+    console.log(carro2)
+    console.log('O resultao é: ', carro1, a*p1)
+    console.log('O resultao é: ', carro2, b*p2)
+}
